594ee9a9f947aff667f1b81f6c33a866963c66e0
shefat salam <mir_salammia@yahoo.com>
2017-10-11 03:46:44 +0600

    javascript

--- js/app.js
+++ js/app.js
@@ -1,18 +1,45 @@
-$(document).ready(function(){
-   $('.single-item').slick({
-      // dots: true,
-      infinite: true,
-      autoplay: true,
-      autoplaySpeed: 6000,
-      speed: 800,
-      slidesToShow: 1,
-      adaptiveHeight: true,
-      arrows: true,
-      prevArrow:"<img class='a-left control-c prev slick-prev' src='images/arrow-left.png'>",
-      nextArrow:"<img class='a-right control-c next slick-next' src='images/arrow-right.png'>",
-      onAfterChange : function() {
-        player.stopVideo();
-      }
-    });
-});
+document.getElementById("myForm").addEventListener("submit", saveBookMark);
 
+
+function saveBookMark(e) {
+  e.preventDefault();
+  var siteName = document.getElementById("sitename").value;
+  var siteUrl = document.getElementById("siteurl").value;
+
+  var bookmark = {
+    name:siteName,
+    url:siteUrl
+  }
+ console.log(bookmark);
+  // Store
+// localStorage.setItem("lastname", "Smith");
+// Retrieve
+// document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+
+  if(localStorage.getItem("bookmarks") === null){
+    var bookmarks = [];
+    bookmarks.push(bookmark);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }else{
+    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    console.log(bookmarks);
+    bookmarks.push(bookmark);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+fetchBookMarsks()
+}
+function fetchBookMarsks(){
+  var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  var bookmarksResult = document.getElementById("bookmarkResults");
+  bookmarksResult.innerHTML="";
+  for (var i = 0; i < bookmarks.length; i++) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+    bookmarksResult.innerHTML += '<div class="well"'+
+                                  '<h3>'+name+
+                                  '<a class="btn btn-default" target="_blank" href="'+url+'">visit</a>'+
+                                  '<a onclick="deletebookmark(\''+url+'\')" class="btn btn-danger" target="_blank" href="#">Delete</a>'+
+                                  '</h3>'+
+                                  '</div>';
+  }
+}
